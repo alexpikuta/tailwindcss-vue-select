f231f075e17ffdccd3bd1d2ae6e958337ebd4fcc
alexpikuta <olexandr.pikuta@gmail.com>
2019-11-27 02:12:30 +0200

    Merge develop branch (#1)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,90 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    >
+    <section
+      class="m-6"
+    >
+      <p
+        class="m-2"
+      >
+        Print only values (<code>print-keys</code> option set to false)
+      </p>
+      <TailwindcssVueSelect
+        v-model="value2"
+        :items="arrayData"
+        :print-keys="false"
+        :divider="'-'"
+      >
+        Label go here
+      </TailwindcssVueSelect>
+      <p>
+        Value: {{ value2 }}
+      </p>
+    </section>
+
+    <section
+      class="m-6"
+    >
+      <p
+        class="m-2"
+      >
+        Print pair key-value (<code>print-keys</code> option set to true)
+      </p>
+      <TailwindcssVueSelect
+        v-model="value1"
+        :items="arrayData"
+        :is-print-keys="true"
+        :divider="'-'"
+      >
+        Label go here
+      </TailwindcssVueSelect>
+      <p>
+        Value: {{ value1 }}
+      </p>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TailwindcssVueSelect from './components/TailwindcssVueSelect.vue'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    TailwindcssVueSelect
+  },
+  data () {
+    return {
+      arrayData: [
+        { 'first key': '1' },
+        { 'second key': 2 },
+        { 'third key': 'three' },
+        { 'fourth key': true },
+        { 'fiveth key': false },
+        { 'sixth key': 0 },
+        { 'seventh key': new Date() }
+      ],
+      value1: ' ',
+      value2: ' '
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+img {
+  margin: 30px auto
+}
+blockquote,
+p {
+  text-align: left
+}
+code {
+  padding: 2px;
+  background-color: #efefef;
+  border-radius: 4px
 }
 </style>
---
+++ src/components/TailwindcssVueSelect.vue
@@ -0,0 +1,111 @@
+<template>
+  <div
+    ref="select"
+    :class="['relative cursor-pointer text-left drop-down',
+             {
+               'drop-down--disabled': isDisabled,
+               'drop-down--active': isOpenList
+             }]"
+    :value="selectedValue"
+  >
+    <div
+      class="relative"
+    >
+      <span
+        v-if="$slots.default"
+        :class="['drop-down__label text-base pointer-events-none',
+                 {
+                   'drop-down__label--floated': selectedValue || isOpenList
+                 }]"
+      >
+        <slot />
+      </span>
+      <input
+        :class="[
+          'bg-white focus:outline-none focus:shadow-outline border rounded-lg py-4 px-4 block w-full appearance-none leading-none',
+          {
+            'drop-down__input--active': isOpenList
+          }
+        ]"
+        type="text"
+        :value="selectedValue"
+        readonly
+        @blur="closeWithBlur"
+        @click="isOpenList = !isOpenList"
+        @keyup.esc="clearWithClosing"
+        @keyup.tab="isOpenList = true"
+      >
+    </div>
+    <transition name="drop-down-toogle">
+      <ul
+        v-if="isOpenList"
+        ref="selectList"
+        class="drop-down__list scroll absolute w-full m-0 p-0 rounded-sm border z-10"
+      >
+        <li
+          v-for="(item, index) in items"
+          :key="index"
+          class="drop-down__item m-0 text-base "
+          @click.stop="setValue(item)"
+        >
+          {{ isPrintKeys ? Object.keys(item)[0] : null }}
+          {{ isPrintKeys ? divider : null }}
+          {{ Object.values(item)[0] }}
+        </li>
+      </ul>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TailwindVueSelect',
+  props: {
+    items: {
+      type: Array,
+      default: null
+    },
+    isPrintKeys: {
+      type: Boolean,
+      default: false
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false
+    },
+    divider: {
+      type: String,
+      default: '-'
+    }
+  },
+  data () {
+    return {
+      isOpenList: false,
+      selectedValue: ''
+    }
+  },
+  methods: {
+    setValue (item) {
+      this.selectedValue = this.isPrintKeys ? Object.keys(item)[0] + ` ${this.divider} ` + Object.values(item)[0] : Object.values(item)[0] + ''
+      this.$emit('input', item)
+    },
+    closeWithBlur () {
+      this.isOpenList = false
+    },
+    clearWithClosing () {
+      this.selectedValue = undefined
+      this.$emit('input', undefined)
+      this.closeWithBlur()
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import "tailwindcss/base"
+@import "tailwindcss/components"
+@import "tailwindcss/utilities"
+
+@import './variables.sass'
+@import './styles.sass'
+</style>
